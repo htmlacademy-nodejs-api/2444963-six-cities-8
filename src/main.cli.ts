4f216f891b9be6f6117eeed92386f69b29af7ccf
Albet Savchenko <art.bertes@gmail.com>
2024-09-29 20:42:44 -0500

    3.10. Prepare application architecture

--- src/main.cli.ts
+++ src/main.cli.ts
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
-import { CLIApplication, GenerateCommand, HelpCommand, ImportCommand, VersionCommand } from './cli/index.js';
+import {
+  CLIApplication,
+  GenerateCommand,
+  HelpCommand,
+  ImportCommand,
+  VersionCommand,
+} from './cli/index.js';
 
 function bootstrap() {
   const cliApplication = new CLIApplication();
